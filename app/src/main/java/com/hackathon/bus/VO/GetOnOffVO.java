b24a232bf99d6d232df609430f88c08d50518401
aerain <j40f893@gmail.com>
2018-08-13 00:47:10 +0900

    VO친구들

---
+++ app/src/main/java/com/hackathon/bus/VO/GetOnOffVO.java
@@ -0,0 +1,13 @@
+package com.hackathon.bus.VO;
+
+public class GetOnOffVO {
+
+    private int busSize;
+    private String busNum;
+    private String sName;
+    private int busTimeUp;
+    private int busTimeDown;
+
+
+
+}
